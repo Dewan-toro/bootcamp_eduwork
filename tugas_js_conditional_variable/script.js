935b5f9ddce08efb7954573b13de36449a16ad69
Dewan-toro <dewantoro477@gmail.com>
2024-02-16 10:08:26 +0700

    feat: tugas js conditional variable & manipulasi data

--- tugas_js_conditional_variable/script.js
+++ tugas_js_conditional_variable/script.js
@@ -27,3 +27,25 @@ const calonPresidents = [
     },
   },
 ];
+
+// Conditional Variable dengan operator ternary
+const statusUsia = calonPresidents[0].umur > 60 ? "Lansia" : "Muda/Tengah";
+console.log(`${calonPresidents[0].nama} memiliki status usia: ${statusUsia}`);
+
+// Conditional Variable dengan operator and
+const statusUsiaAnd = calonPresidents[1].umur > 60 && "Lansia";
+console.log(
+  `${calonPresidents[1].nama} memiliki status usia: ${statusUsiaAnd}`
+);
+
+// Conditional Variable dengan operator or
+const statusUsiaOr = calonPresidents[2].umur > 60 || "Sat-set";
+console.log(`${calonPresidents[2].nama} memiliki status usia: ${statusUsiaOr}`);
+
+// Manipulasi data dengan map
+const namaCapres = calonPresidents.map((capres) => capres.nama);
+console.log(namaCapres);
+
+// Manipulasi data dengan filter
+const capresMuda = calonPresidents.filter((capres) => capres.umur < 60);
+console.log(capresMuda);
