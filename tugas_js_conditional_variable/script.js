27a66a75cd38b800094ea7fa0efbb8062e5cb321
Dewan-toro <dewantoro477@gmail.com>
2024-02-16 09:19:51 +0700

    feat: tugas eduwork

---
+++ tugas_js_conditional_variable/script.js
@@ -0,0 +1,29 @@
+const calonPresidents = [
+  {
+    no: 1,
+    nama: "H. ANIES RASYID BASWEDAN, PH.D",
+    umur: 54,
+    ttl: {
+      tempat: "Kuningan",
+      tgl: "7 Mei 1969",
+    },
+  },
+  {
+    no: 2,
+    nama: "H. PRABOWO SUBIANTO",
+    umur: 72,
+    ttl: {
+      tempat: "Jakarta",
+      tgl: "17 Oktober 1951",
+    },
+  },
+  {
+    no: 3,
+    nama: "H. GANJAR PRANOWO, S,H. M,LPe",
+    umur: 55,
+    ttl: {
+      tempat: "Karanganyar",
+      tgl: "28 Oktober 1968",
+    },
+  },
+];
